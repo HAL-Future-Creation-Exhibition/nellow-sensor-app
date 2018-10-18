724e24260d633cc7f5a2e0ddf22200c701eaa911
tanukiti <33892948+tanukiti@users.noreply.github.com>
2018-10-18 09:27:46 +0900

    commit

---
+++ nellow-sensor-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nellow-sensor-app
+//
+//  Created by 高鉾大貴 on 2018/10/18.
+//  Copyright © 2018 高鉾大貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
