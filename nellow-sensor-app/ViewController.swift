5ff4650d6104c9a283dd8d77dfc920f674abe260
tanukiti <33892948+tanukiti@users.noreply.github.com>
2018-10-20 16:50:06 +0900

    通信でけた！

--- nellow-sensor-app/ViewController.swift
+++ nellow-sensor-app/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    let myDevice: UIDevice = UIDevice.current
+    let flg: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        myDevice.isProximityMonitoringEnabled = true
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(proximitySensorStateDidChange), name: UIDevice.proximityStateDidChangeNotification, object: nil)
     }
-
+    
+    @objc func proximitySensorStateDidChange() {
+        print(myDevice.proximityState)
+        if myDevice.proximityState {
+            label.text = "ちかずいたよ"
+            Alamofire.request("https://ac31deed.ngrok.io/sleep", method: .post, parameters: nil, encoding: JSONEncoding.default, headers: nil)
+        } else {
+            label.text = "離れたよ"
+            Alamofire.request("https://ac31deed.ngrok.io/wakeup", method: .post, parameters: nil, encoding: JSONEncoding.default, headers: nil)
+        }
+    }
+    
+    
+    
 
 }
 
